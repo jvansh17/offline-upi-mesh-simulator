404566a699f5044d82e4d2d6f2273c841ec7ebd5
Vansh Jain <0251MCA222@niet.co.in>
2026-08-01 02:34:54 +0530

    feat: initialize Spring Boot project with PostgreSQL

---
+++ src/main/java/com/vansh/offlineupimesh/OfflineUpiMeshApplication.java
@@ -0,0 +1,13 @@
+package com.vansh.offlineupimesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflineUpiMeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfflineUpiMeshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vansh/offlineupimesh/OfflineUpiMeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vansh.offlineupimesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfflineUpiMeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
