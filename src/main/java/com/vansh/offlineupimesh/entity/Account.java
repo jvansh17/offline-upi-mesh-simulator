9ca6ba4c176914980635018007c70e01816ca631
Vansh Jain <0251MCA222@niet.co.in>
2026-08-03 16:46:19 +0530

    Create Account entity with constructors and accessors

---
+++ src/main/java/com/vansh/offlineupimesh/entity/Account.java
@@ -0,0 +1,62 @@
+package com.vansh.offlineupimesh.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String accountNumber;
+
+    @Column(nullable = false)
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    // Constructors
+
+    public Account(){
+
+    }
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    // Getters
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+
+    // Setters
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAccountNumber(String accountNumber){
+        this.accountNumber = accountNumber;
+    }
+
+    // toString()
+}
